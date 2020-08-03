6e80c8fe65983feeb7108428efe8cf121ec09bb1
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-08-03 19:43:06 -0300

    Iniciando Funcionalidades

--- labeddit/src/components/PageSignup/PageSignup.js
+++ labeddit/src/components/PageSignup/PageSignup.js
@@ -1,20 +1,48 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
 
+const useForm = initialValues => {
+    const [form, setForm] = useState(initialValues);
+  
+    const onChange = (name, value) => {
+      const newForm = { ...form, [name]: value };
+      setForm(newForm);
+    };
+  
+    return { form, onChange };
+};
+  
 function PageSignup() {
+    const { form, onChange } = useForm({
+        nomeUsuario: "",
+        email: "",
+        senha: ""
+      });
 
-    const history = useHistory()
+    const history = useHistory();
+
+    const handleInputChange = event => {
+        const { name, value} = event.target;
     
+        onChange(name, value);
+    };
+
     const goToLogin = () => {
         history.push("/")
     }
 
     return (
         <div>
+            <button onClick={goToLogin}>Voltar</button>
             PageSignup
-            <button onClick={goToLogin}>Cadastrar</button>
+            <form>
+                <input onChange={handleInputChange} name={"nomeUsuario"} value={form.nomeUsuario} placeholder={"Nome de usuário"} type={"text"}/>
+                <input onChange={handleInputChange} name={"email"} value={form.email} placeholder={"Email"} type={"email"}/>
+                <input onChange={handleInputChange} name={"senha"} value={form.senha} placeholder={"Senha"} type={"password"}/>
+                <button onClick={goToLogin}>Cadastrar</button>
+            </form>
         </div>
     )
 }
 
-export default PageSignup
+export default PageSignup;
