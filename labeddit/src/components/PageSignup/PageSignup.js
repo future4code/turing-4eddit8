12b0ec5a5885b67eb96f5263c146366480ee92b9
Vinícius Fredeanelle <fredaummmassis@gmail.com>
2020-08-04 14:37:32 -0300

    Requisição de cadastro funcionando

--- labeddit/src/components/PageSignup/PageSignup.js
+++ labeddit/src/components/PageSignup/PageSignup.js
@@ -1,5 +1,8 @@
 import React, {useState, useEffect} from 'react'
 import {useHistory} from 'react-router-dom'
+import axios from 'axios';
+
+const url = "https://us-central1-labenu-apis.cloudfunctions.net/labEddit"
 
 const useForm = initialValues => {
     const [form, setForm] = useState(initialValues);
@@ -31,15 +34,33 @@ function PageSignup() {
         history.push("/")
     }
 
+    const cadastrar=(event)=>{
+        event.preventDefault()
+        const body ={
+            "email": form.email,
+            "password": form.senha,
+            "username": form.nomeUsuario
+        }
+        axios
+        .post(`${url}/signup`, body)
+        .then(response=>{
+            console.log(response.data)
+            history.push("/")
+        })
+        .catch(err=>{
+            console.log(err.message)
+        })
+    }
+
     return (
         <div>
             <button onClick={goToLogin}>Voltar</button>
             PageSignup
-            <form>
-                <input onChange={handleInputChange} name={"nomeUsuario"} value={form.nomeUsuario} placeholder={"Nome de usuário"} type={"text"}/>
-                <input onChange={handleInputChange} name={"email"} value={form.email} placeholder={"Email"} type={"email"}/>
-                <input onChange={handleInputChange} name={"senha"} value={form.senha} placeholder={"Senha"} type={"password"}/>
-                <button onClick={goToLogin}>Cadastrar</button>
+            <form onSubmit={cadastrar}>
+                <input onChange={handleInputChange} name={"nomeUsuario"} value={form.nomeUsuario} placeholder={"Nome de usuário"} type={"text"} required/>
+                <input onChange={handleInputChange} name={"email"} value={form.email} placeholder={"Email"} type={"email"} required/>
+                <input onChange={handleInputChange} name={"senha"} value={form.senha} placeholder={"Senha"} type={"password"} required/>
+                <button>Cadastrar</button>
             </form>
         </div>
     )
